a0e78aea56cffcd288b2f65376f6aba377b722ef
Dustin Johnson <djohnson@machenergy.com>
2017-07-24 09:43:47 -0500

    Add environment variables w/ secrets

--- handler.js
+++ handler.js
@@ -7,11 +7,6 @@ let reddit = new Reddit();
 // Example invocation:
 // curl -X 'POST' <url> -H 'Content-Type: application/json' -d '{"title" : "Some Title", "url" : "http://thisisaurl.com", "description" : "Some cool stuff happened" }'
 module.exports.newRedditPost = (event, context, callback) => {
-    console.log('Body:', event.body);
-    console.log('Headers:', event.headers);
-    console.log('Method:', event.method);
-    console.log('Params:', event.params);
-    console.log('Query:', event.query);
 
     reddit.newPost(event.body, (err) => {
         if (err) {
